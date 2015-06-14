faf0e168abc2127c2d14a0ffb4c5365faf2c3b3e
Mithun Sasidharan <sasidham@advisory.com>
2015-06-14 07:56:03 +0530

    Data load moved outside of module

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 "use strict";
 
+var pincodeData = require('./pincodes.json');
+
 function isNumeric(num) {
     return !isNaN(num);
 }
 
 module.exports = {
-
-    pincodeData: require('./pincodes.json'),
-
     lookup: function lookup(pincode) {
         if (isNumeric(pincode)) {
             if (typeof pincode === 'string') {
-                return this.pincodeData.filter(function(e) {
+                return pincodeData.filter(function(e) {
                     return e.pincode === +pincode;
                 });
             } else if (typeof pincode === 'number') {
-                return this.pincodeData.filter(function(e) {
+                return pincodeData.filter(function(e) {
                     return e.pincode === pincode;
                 });
             }
         } else {
             var regex = RegExp(pincode, 'i');
-            return this.pincodeData.filter(function(e) {
+            return pincodeData.filter(function(e) {
                 return e.officeName.match(regex);
             });
         }
