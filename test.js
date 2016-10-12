b0ee4f5cf829712f403845b69172a5f304d87afd
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2016-10-12 22:25:40 +0530

    make sure tests assert

--- test.js
+++ test.js
@@ -1,12 +1,28 @@
 "use strict";
 
+var assert = require('assert');
 var pincodeDirectory = require('./index.js')
 
-pincodeDirectory.lookup('hinjawadi');
-//[{officeName: 'Infotech  Park (Hinjawadi) S.O',pincode: 411057,taluk: 'Haveli',districtName: 'Pune',stateName: 'MAHARASHTRA'}]
+assert.deepStrictEqual(pincodeDirectory.lookup('hinjawadi'), [{
+  officeName: 'Infotech  Park (Hinjawadi) S.O',
+  pincode: 411057,
+  taluk: 'Haveli',
+  districtName: 'Pune',
+  stateName: 'MAHARASHTRA'
+}]);
 
-pincodeDirectory.lookup(682028); 
-//[{officeName: 'Vennala S.O',pincode: 682028,taluk: 'Ernakulam',districtName: 'Ernakulam',stateName: 'KERALA'}]
+assert.deepStrictEqual(pincodeDirectory.lookup(682028), [{
+  officeName: 'Vennala S.O',
+  pincode: 682028,
+  taluk: 'Ernakulam',
+  districtName: 'Ernakulam',
+  stateName: 'KERALA'
+}]);
 
-pincodeDirectory.lookup('600096'); 
-//[{officeName: 'Perungudi S.O',pincode: 600096,taluk: 'Saidapet',districtName: 'Kanchipuram',stateName: 'TAMIL NADU'}]
+assert.deepStrictEqual(pincodeDirectory.lookup('600096'), [{
+  officeName: 'Perungudi S.O',
+  pincode: 600096,
+  taluk: 'Saidapet',
+  districtName: 'Kanchipuram',
+  stateName: 'TAMIL NADU'
+}]);
