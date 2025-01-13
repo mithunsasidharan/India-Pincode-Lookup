cd642a4ab8ba05d8441682debc33dfe70f305728
Vishal Gupta <vishalsg42@gmail.com>
2025-01-13 23:23:55 +0530

    configured husky & formatted code

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/test/**/*.test.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
   moduleNameMapper: {
-    "^@app/(.*)$": "<rootDir>/src/$1"
+    '^@app/(.*)$': '<rootDir>/src/$1',
   },
 };
